a032804a99661b409f596ab76113aee69dce09db
Kunal Parihar <kunal.parihar2494@gmail.com>
2025-07-11 14:23:23 +0530

    error resolve

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -23,7 +23,7 @@ const userAuth = async (req, res, next) => {
     req.user = user;
     next();
   } catch (error) {
-    res.status(400).send("Error: " + error.message);
+    res.status(400).json({"Error": + error.message});
   }
 };
 
